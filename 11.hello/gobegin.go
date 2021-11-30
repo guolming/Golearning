515ac1c5432a744a10c5fde56c89fd6481b9e49f
guolming <345480708@qq.com>
2021-11-30 12:12:54 +0800

    夜雨寄北

---
+++ 11.hello/gobegin.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("夜雨寄北")
+	fmt.Println("君问归期未有期，")
+	fmt.Println("巴山夜雨涨秋池，")
+	fmt.Println("何当共剪西窗烛，")
+	fmt.Println("却话巴山夜雨时。")
+}
