294e3fe9df7e46b144d4936a60f6c0f55b8c7160
guolming <345480708@qq.com>
2021-12-01 11:56:14 +0800

    增加字符串输出

---
+++ strfmt/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("中文，abc，1234，❥(^_-)拜托 拜托了")
+	fmt.Printf("我的名字是%s\n", "小强")
+	fmt.Printf("我的名字是%q\n", "小强")
+	fmt.Printf("我的名字是%x\n", "小强")
+	fmt.Printf("我的名字是%X\n", "小强")
+}
