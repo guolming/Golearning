f6fd2a97f5b0993be9e60ab63fcf90c810133be9
guolming <345480708@qq.com>
2021-12-01 11:53:41 +0800

    增加calcu

---
+++ calcu/calsign.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a, b int = 3, 66
+	fmt.Println("a + b=", a+b)
+	fmt.Println("a - b=", a-b)
+	fmt.Println("a * b=", a*b)
+	fmt.Println("a / b=", a/b)
+	fmt.Println("a % b=", a%b)
+
+}
