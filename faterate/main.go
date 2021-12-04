65db2eb00cd4ab83909c5f38627db678ccb44e97
guolming <345480708@qq.com>
2021-12-04 13:14:39 +0800

    fateRate体脂率固定输出

---
+++ faterate/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var weight float64 = 67.0
+	var tall float64 = 1.7
+	var age int = 37
+	var BMI float64 = weight / (tall * tall)
+	var sex string = "男"
+	var sexWeight int
+	if sex == "男" {
+		sexWeight = 1
+	} else {
+		sexWeight = 0
+	}
+	var fateRate float64 = (1.2*BMI + 0.23*float64(age) - 5.4 - 10.8*float64(sexWeight)) / 100
+	fmt.Println("体脂率是：", fateRate)
+
+	if sex == "男" {
+		//todo 编写男性体脂率和体脂状态表
+		if age >= 18 && age <= 39 {
+			if fateRate >= 0 && fateRate <= 0.1 {
+				fmt.Println("目前是：偏瘦，要多吃多锻炼，增强体质。")
+			} else if fateRate > 0.1 && fateRate <= 0.16 {
+				fmt.Println("目前是：标准，太棒了，要继续保持哦！")
+			} else if fateRate > 0.16 && fateRate <= 0.21 {
+				fmt.Println("目前是：偏重，吃完饭要多散散步，消化消化。")
+			} else if fateRate > 0.21 && fateRate <= 0.26 {
+				fmt.Println("目前是：偏胖，少吃点，多运动运动。")
+			} else if fateRate > 0.26 {
+				fmt.Println("目前是：非常肥胖，健身游泳跑步，即刻开始。")
+			}
+		} else if age > 39 && age <= 59 {
+			//todo
+		} else if age > 59 {
+			//todo
+		} else {
+			fmt.Println("我们不看未成年人的体脂率，变化太大，无法评判")
+		}
+	} else {
+		//todo 编写女性体脂率和体脂状态表
+	}
+
+}
